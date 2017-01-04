8a62ee62388c8a3932e399f9d1448e23a229af1f
Alucia <analu1478@gmail.com>
2017-01-04 06:18:18 -0500

    ingresando nombre

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
-
-    function Reloj() {
-        var tiempo = new Date();
-        var hora = tiempo.getHours();
-        var minuto = tiempo.getMinutes();
-        var segundo = tiempo.getSeconds();
-        document.getElementById('hora').innerHTML = hora;
-        document.getElementById('minuto').innerHTML = minuto;
-        document.getElementById('segundo').innerHTML = segundo;
-        // setTimeout('Reloj()', 1000);
-        str_hora = new String(hora);
-        if (str_hora.length == 1) {
-            document.getElementById('hora').innerHTML = '0' + hora;
-        }
-        str_minuto = new String(minuto);
-        if (str_minuto.length == 1) {
-            document.getElementById('minuto').innerHTML = '0' + minuto;
-        }
-        str_segundo = new String(segundo);
-        if (str_segundo.length == 1) {
-            document.getElementById('segundo').innerHTML = '0' + segundo;
-        }
-        if (hora < 12){
-            prompt("buenos días","escribe tú nombre");
-        }
-        else if (hora < 19){
-            var nombre = prompt("buenas tardes","escribe tú nombre");
-        }
-        else {
-            prompt("buenas noches","escribe tú nombre");
-        }
+function Reloj() {
+    var tiempo = new Date();
+    var hora = tiempo.getHours();
+    var minuto = tiempo.getMinutes();
+    var segundo = tiempo.getSeconds();
+    document.getElementById('hora').innerHTML = hora;
+    document.getElementById('minuto').innerHTML = minuto;
+    document.getElementById('segundo').innerHTML = segundo;
+    // setTimeout('Reloj()', 1000);
+    str_hora = new String(hora);
+    if (str_hora.length == 1) {
+        document.getElementById('hora').innerHTML = '0' + hora;
     }
+    str_minuto = new String(minuto);
+    if (str_minuto.length == 1) {
+        document.getElementById('minuto').innerHTML = '0' + minuto;
+    }
+    str_segundo = new String(segundo);
+    if (str_segundo.length == 1) {
+        document.getElementById('segundo').innerHTML = '0' + segundo;
+    }
+    if (hora < 12){
+        document.getElementById('mostrarSaludo').innerHTML = "Buenos Dias," ;
+    }
+    else if (hora < 19){
+        document.getElementById('mostrarSaludo').innerHTML = "Buenas Tardes," ;
+    }
+    else {
+        document.getElementById('mostrarSaludo').innerHTML = "Buenas Noches," ;
+    }
+}
+Reloj();
 
-    Reloj();
+function nameletra(name){
+    
+}
+
