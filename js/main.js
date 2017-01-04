1eafa83d2ee4367138f8abb0afa7fa444a875ccf
Alucia <analu1478@gmail.com>
2017-01-03 22:10:53 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-function Reloj() {
-    var tiempo = new Date();
-    var hora = tiempo.getHours();
-    var minuto = tiempo.getMinutes();
-    var segundo = tiempo.getSeconds();
-    document.getElementById('hora').innerHTML = hora;
-    document.getElementById('minuto').innerHTML = minuto;
-    document.getElementById('segundo').innerHTML = segundo;
-    // setTimeout('Reloj()', 1000);
-    str_hora = new String(hora);
-    if (str_hora.length == 1) {
-        document.getElementById('hora').innerHTML = '0' + hora;
-    }
-    str_minuto = new String(minuto);
-    if (str_minuto.length == 1) {
-        document.getElementById('minuto').innerHTML = '0' + minuto;
-    }
-    str_segundo = new String(segundo);
-    if (str_segundo.length == 1) {
-        document.getElementById('segundo').innerHTML = '0' + segundo;
-    }
-    if (hora < 12){
-    	alert("buenos días");
-    }
-    else if ( hora >= 12 && hora < 19){
-    	alert("buenas tardes");
-    }
-    else {
-    	alert("buenas noches");
-    }
-}
 
-Reloj();
+    function Reloj() {
+        var tiempo = new Date();
+        var hora = tiempo.getHours();
+        var minuto = tiempo.getMinutes();
+        var segundo = tiempo.getSeconds();
+        document.getElementById('hora').innerHTML = hora;
+        document.getElementById('minuto').innerHTML = minuto;
+        document.getElementById('segundo').innerHTML = segundo;
+        // setTimeout('Reloj()', 1000);
+        str_hora = new String(hora);
+        if (str_hora.length == 1) {
+            document.getElementById('hora').innerHTML = '0' + hora;
+        }
+        str_minuto = new String(minuto);
+        if (str_minuto.length == 1) {
+            document.getElementById('minuto').innerHTML = '0' + minuto;
+        }
+        str_segundo = new String(segundo);
+        if (str_segundo.length == 1) {
+            document.getElementById('segundo').innerHTML = '0' + segundo;
+        }
+        if (hora < 12){
+            prompt("buenos días","escribe tú nombre");
+        }
+        else if (hora < 19){
+            var nombre = prompt("buenas tardes","escribe tú nombre");
+        }
+        else {
+            prompt("buenas noches","escribe tú nombre");
+        }
+    }
+
+    Reloj();
